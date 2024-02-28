d9c21d89644702e1e30a8c3f34d45e30f00c645b
David Eastmond <davebuck1981@gmail.com>
2024-02-28 18:37:50 -0500

    refactor and updates related to spaces/s3

--- travel-event-host-app/src/app/integration/utils/generate-filename.ts
+++ travel-event-host-app/src/app/integration/utils/generate-filename.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-export function generateFilename(userId: string): string {
+export function generateFilename(id: string): string {
   const randomMongoId = new mongoose.Types.ObjectId();
-  return `${userId}-${randomMongoId.toString()}`;
+  return `${id}-${randomMongoId.toString()}`;
 }
