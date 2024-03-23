a240007366688a91e12b367540dbe8faf0f587db
David Eastmond <davebuck1981@gmail.com>
2024-03-23 18:58:26 -0400

    enhance message renderer

--- travel-event-host-app/src/components/messaging/message-renderer/MessageRenderer.tsx
+++ travel-event-host-app/src/components/messaging/message-renderer/MessageRenderer.tsx
@@ -1,8 +1,13 @@
 import { UserClient } from '@/app/clients/user/user-client';
 import { MessageThread } from '@/models/messaging/message-thread.model';
 import { SecureUser } from '@/types/secure-user';
+import dayjs from 'dayjs';
 import { useEffect, useState } from 'react';
-import { MessageBlurb, MessageBlurbContainer } from '../message-thread-card/MessageThreadCard';
+import {
+  MessageBlurb,
+  MessageBlurbContainer,
+  TimestampStrip,
+} from '../message-thread-card/MessageThreadCard';
 
 export function MessageRenderer({
   threadContext,
@@ -24,20 +29,59 @@ export function MessageRenderer({
     fetchContextUsers();
   }, []);
 
-  // Then render the messages
-  return (
-    contextUsers &&
-    threadContext.messages.map((message, index) => {
+  /*
+    This logic is responsible for rendering the messages in the message thread
+  */
+  const render = () => {
+    const messageElements: JSX.Element[] = [];
+    let currentDate = null;
+    if (threadContext.messages?.length > 0) {
+      currentDate = threadContext.messages[0].timestamp;
+      messageElements.push(
+        <TimestampStrip
+          key={currentDate.toString()}
+          dateString={calculateDateCaption(currentDate)}
+        />,
+      );
+    }
+
+    // Iterate through each message in the thread and render it
+    for (const message of threadContext.messages) {
+      // If the current message's date is different from the previous message's date, add a new timestamp strip
+      if (currentDate && !dayjs(message.timestamp).isSame(dayjs(currentDate), 'day')) {
+        currentDate = message.timestamp;
+        messageElements.push(
+          <TimestampStrip
+            key={currentDate.toString()}
+            dateString={calculateDateCaption(currentDate)}
+          />,
+        );
+      }
       const position = message.sender === userId ? 'end' : 'start';
-      return (
+
+      messageElements.push(
         <MessageBlurbContainer position={position} key={message._id}>
           <MessageBlurb
             body={message.body}
             position={position}
-            user={contextUsers.find((user) => user._id === message.sender) as SecureUser}
+            user={contextUsers?.find((user) => user._id === message.sender) as SecureUser}
+            timestamp={message.timestamp}
           />
-        </MessageBlurbContainer>
+        </MessageBlurbContainer>,
       );
-    })
-  );
+    }
+    return messageElements;
+  };
+
+  // iterate through each message and grab its created date.
+  return contextUsers && render();
+}
+
+function calculateDateCaption(timestamp: Date): string {
+  // This function will calculate how to display a date
+
+  if (dayjs().isSame(dayjs(timestamp), 'day')) {
+    return 'Today';
+  }
+  return dayjs(timestamp).format('MMM D, YYYY');
 }
